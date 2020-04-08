0cbe5f85c51fcb459ef83edbfae88240cc6ea710
1405 <swkoo9311@gmail.com>
2020-04-08 14:13:47 +0900

    gitignore 등록

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+   // @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+   // List<Posts> findAllDesc();
+}
